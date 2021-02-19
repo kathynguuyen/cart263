b3b56393813b4366023e31d56800ea8873fa9f44
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-02-19 03:07:52 -0500

    A3: setting up handmodel

--- activities/bubble-popper/js/script.js
+++ activities/bubble-popper/js/script.js
@@ -1,12 +1,15 @@
 "use strict";
 
 /**
-Title of Project
-Author Name
+Kathy Nguyen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Activity 4 : Bubble Popper
 */
+
+// user's webcam
+let video = undefined;
+
+let predictions = [];
 
 /**
 Description of preload
@@ -21,6 +24,24 @@ Description of setup
 */
 function setup() {
 
+  createCanvas(640,480);
+
+  // access user's webcam
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // load the handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  },   function() {
+      console.log(`Model loaded.`);
+    });
+
+    // listen for predictions
+    handpose.on(`predict`, function (results) {
+      console.log(results);
+      predictions = results;
+    });
 }
 
 
@@ -28,5 +49,10 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  if (predictions.length > 0) {
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+  }
 }
