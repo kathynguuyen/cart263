154a52bd43c04f8a37cf59276ca96ea7be079a07
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-02-19 03:21:27 -0500

    A4: Added bubble and pop

--- activities/bubble-popper/js/script.js
+++ activities/bubble-popper/js/script.js
@@ -9,7 +9,15 @@ Activity 4 : Bubble Popper
 // user's webcam
 let video = undefined;
 
+// handpose model
+let handpose = undefined;
+
+// current set of predictions
 let predictions = [];
+
+
+// the Bubble
+let bubble = undefined;
 
 /**
 Description of preload
@@ -42,6 +50,15 @@ function setup() {
       console.log(results);
       predictions = results;
     });
+
+    // our Bubble
+    bubble = {
+      x: random(width),
+      y: height,
+      size: 100,
+      vx: 0,
+      vy: -2
+    };
 }
 
 
@@ -54,5 +71,50 @@ function draw() {
   if (predictions.length > 0) {
     let hand = predictions[0];
     let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+
+    // pin body
+    push();
+    noFill();
+    stroke(255,255,255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop();
+
+
+    // pin head
+    push();
+    noStroke();
+    fill(255,0,0);
+    ellipse(baseX, baseY, 20);
+    pop();
+
+    // check bubble popping
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size/2) {
+      bubble.x = random(width);
+      bubble.y = height;
+    }
   }
+
+  // move the Bubble
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+
+  if (bubble.y < 0) {
+    bubble.x = random(width);
+    bubble.y = height;
+  }
+
+  push();
+  fill(0,100,200);
+  noStroke();
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+
 }
