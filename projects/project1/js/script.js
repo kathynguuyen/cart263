3c3674502c758a82e6615ccb82c5130369edbaa0
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-03-03 02:38:13 -0500

    P1: Added a second rocket

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,10 +9,10 @@ Project 01: Inspired by the korean movie Space Sweepers.
 // agent profile (login and password)
 let agentProfile = {
   name: `**REDACTED**`,
-}
+};
 
 let gameData = {
-  highScore: 0
+  highScore: 0,
 };
 
 // user's webcam
@@ -32,6 +32,7 @@ let rock = undefined;
 
 // rocket (obstacle)
 let rocket = undefined;
+let secondRocket = undefined;
 
 let rockSFX;
 
@@ -47,21 +48,17 @@ let backgroundImg;
 let rockImg;
 let secondRocketImg;
 
-
-
 /**
 Description of preload
 Load fonts, pictures and sounds
 */
 function preload() {
-
   // load sounds
   rockSFX = loadSound(`assets/sounds/popSound.mp3`);
 
   // load fonts
   titleFont = loadFont(`assets/fonts/Awakenning.ttf`);
   paragraphFont = loadFont(`assets/fonts/Petrichor.ttf`);
-
 
   // load images
   rocketImg = loadImage("assets/images/rocket.png");
@@ -70,78 +67,74 @@ function preload() {
   secondRocketImg = loadImage("assets/images/rocket2.png");
 }
 
-
 /**
 Description of setup
 */
 function setup() {
-
-
-  createCanvas(640,480);
+  createCanvas(640, 480);
   // access user's webcam
   video = createCapture(VIDEO);
   video.hide();
 
-
-  let data = JSON.parse(localStorage.getItem('space-sweeper-high-score-data'));
-  if(data !== null) {
+  let data = JSON.parse(localStorage.getItem("space-sweeper-high-score-data"));
+  if (data !== null) {
     gameData = data;
   }
 
   // load the handpose model
-  handpose = ml5.handpose(video, {
-    flipHorizontal: true
-  },   function() {
+  handpose = ml5.handpose(
+    video,
+    {
+      flipHorizontal: true,
+    },
+    function () {
       console.log(`Model loaded.`);
       state = `title`;
-    });
-
-    // listen for predictions
-    handpose.on(`predict`, function (results) {
-      console.log(results);
-      predictions = results;
-    });
-
-    // our rock
-    rock = {
-      x: random(width),
-      y: height,
-      size: 50,
-      vx: 0,
-      vy: -2,
-      img: rockImg
-    };
-
-    // rocket
-      rocket = {
-        x: random(width),
-        y: height,
-        size: 50,
-        vx: 0,
-        vy: -2
-      }
-
-      seconeRocket = {
-        x: random(width),
-        y: height,
-        size: 50,
-        vx: 0,
-        vy: -2
-      }
-
-
-
-    // annyang commands
-    if(annyang) {
-      let commands = {
-        'My name is *name': nameInput
-      };
-      annyang.addCommands(commands);
-      annyang.start();
     }
-1
-}
+  );
 
+  // listen for predictions
+  handpose.on(`predict`, function (results) {
+    console.log(results);
+    predictions = results;
+  });
+
+  // our rock
+  rock = {
+    x: random(width),
+    y: height,
+    size: 50,
+    vx: 0,
+    vy: -2,
+    img: rockImg,
+  };
+
+  // rocket
+  rocket = {
+    x: random(width),
+    y: height,
+    size: 50,
+    vx: 0,
+    vy: -2,
+  };
+
+  secondRocket = {
+    x: random(width),
+    y: height,
+    size: 50,
+    vx: 0,
+    vy: -2,
+  };
+
+  // annyang commands
+  if (annyang) {
+    let commands = {
+      "My name is *name": nameInput,
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+  };
+}
 
 /**
 Description of draw()
@@ -160,27 +153,25 @@ function draw() {
   } else if (state === `lose`) {
     lose();
   }
-
 }
 
-
 /** function loading ---------------------------------------------------
-*/
+ */
 function loading() {
   push();
   textFont(paragraphFont);
   textSize(30);
-  fill(255,255,255);
-  textAlign(CENTER,CENTER);
+  fill(255, 255, 255);
+  textAlign(CENTER, CENTER);
   text(`Space Sweeper Agent Loading...`, width / 2, height / 2);
   pop();
 }
 
 /** end of loading  ---------------------------------------------------
-*/
+ */
 
 /** function title ------------------------------------------------------
-*/
+ */
 function title() {
   push();
   displayTitle();
@@ -188,60 +179,60 @@ function title() {
   pop();
 }
 
-
 // display the main title of the game
-function displayTitle(){
+function displayTitle() {
   textFont(titleFont);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   textSize(50);
-  fill(255,255,255);
+  fill(255, 255, 255);
   text(`SPACE SWEEPERS`, width / 2, height / 2 - 150);
 }
 
 function helloAgent() {
   textFont(paragraphFont);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   textSize(15);
-  fill(255,255,255);
+  fill(255, 255, 255);
   text(`Hello, Space Sweeper Agent! `, width / 2, height / 2 - 40);
-  text(`Welcome to your first day at Space Sweeper, please say: 'My name is '______' `, width/ 2, height / 2);
+  text(
+    `Welcome to your first day at Space Sweeper, please say: 'My name is '______' `,
+    width / 2,
+    height / 2
+  );
 }
 
-
 function nameInput(name) {
-  if(state === `title`) {
-
+  if (state === `title`) {
     agentProfile.name = name.toUpperCase();
     console.log(agentProfile.name);
     state = `instructions`;
     generateAgentProfile();
-
   }
 }
 
-
 /** end of function title -----------------------------------------------
-*/
-
+ */
 
 /** function instructions  ------------------------------------------------------
-*/
+ */
 function instructions() {
   push();
   textFont(paragraphFont);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   textSize(15);
-  fill(255,255,255);
+  fill(255, 255, 255);
   text(`Hello, ${agentProfile.name}`, width / 2, height / 2 - 40);
-  text(`Collect the asteroids with your index finger using your camera!`, width / 2, height / 2);
+  text(
+    `Collect the asteroids with your index finger using your camera!`,
+    width / 2,
+    height / 2
+  );
   text(`Avoid the rockets!`, width / 2, height / 2 + 30);
   text(`Press any key to continue`, width / 2, height / 2 + 50);
   pop();
 }
 
-
 function running() {
-
   push();
   image(backgroundImg, 0, 0, 640, 480);
   fill(0);
@@ -250,7 +241,6 @@ function running() {
   text("High score:" + gameData.highScore, 100, 100);
   text("Current score:" + counter, 100, 150);
   pop();
-
 
   if (predictions.length > 0) {
     let hand = predictions[0];
@@ -265,52 +255,47 @@ function running() {
     // pin body
     push();
     noFill();
-    stroke(255,255,255);
+    stroke(255, 255, 255);
     strokeWeight(2);
     line(baseX, baseY, tipX, tipY);
     pop();
 
-
     // pin head
     push();
     noStroke();
-    fill(255,0,0);
+    fill(255, 0, 0);
     ellipse(baseX, baseY, 50, 50);
     pop();
-
-
 
     // check rock destroy
     let d = dist(tipX, tipY, rock.x, rock.y);
 
-
-    if (d < rock.size/2) {
+    if (d < rock.size / 2) {
       rock.x = random(width);
       rock.y = height;
       counter++;
 
-      if(counter > gameData.highScore) {
+      if (counter > gameData.highScore) {
         gameData.highScore = counter;
-        localStorage.setItem(`space-sweeper-high-score-data`, JSON.stringify(gameData));
+        localStorage.setItem(
+          `space-sweeper-high-score-data`,
+          JSON.stringify(gameData)
+        );
       }
       // sound effect when rock pops
       rockSFX.play();
     }
 
     let d2 = dist(tipX, tipY, rocket.x, rocket.y);
-    if (d2 < rocket.size/2) {
+    if (d2 < rocket.size / 2) {
       state = `lose`;
     }
 
     let d3 = dist(tipX, tipY, secondRocket.x, secondRocket.y);
-    if (d3 < secondRocket.size/2) {
+    if (d3 < secondRocket.size / 2) {
       state = `lose`;
     }
-
   }
-
-
-
 
   // move the rock
   rock.x += rock.vx;
@@ -329,42 +314,33 @@ function running() {
     rock.y = height;
   }
 
+  if (rocket.y < 0) {
+    rocket.x = random(width);
+    rocket.y = height;
+  }
 
-    if (rocket.y < 0) {
-      rocket.x = random(width);
-      rocket.y = height;
-}
-
-
-if (secondRocket.y < 0) {
-  secondRocket.x = random(width);
-  SecondRocket.y = height;
-}
-
-
-
+  if (secondRocket.y < 0) {
+    secondRocket.x = random(width);
+    SecondRocket.y = height;
+  }
 
   push();
-  fill(0,100,200);
+  fill(0, 100, 200);
   noStroke();
-  image(rockImg,rock.x, rock.y, 50,50);
+  image(rockImg, rock.x, rock.y, 50, 50);
   pop();
 
+  image(rocketImg, rocket.x, rocket.y, 60, 60);
 
-
-  image(rocketImg,rocket.x, rocket.y, 60,60);
-
-    image(secondRocketImg,secondRocket.x, secondRocket.y, 70,70);
-
-
+  image(secondRocketImg, secondRocket.x, secondRocket.y, 70, 70);
 }
 
 function lose() {
   push();
   textFont(paragraphFont);
-  textAlign(CENTER,CENTER);
+  textAlign(CENTER, CENTER);
   textSize(15);
-  fill(255,255,255);
+  fill(255, 255, 255);
   text(`Hello, ${agentProfile.name}`, width / 2, height / 2 - 40);
   text(`YOU LOST!`, width / 2, height / 2);
   text(`Press ENTER to restart!`, width / 2, height / 2 + 30);
@@ -378,7 +354,7 @@ function keyPressed() {
   } else if (state === `instructions`) {
     state = `running`;
   } else if (state === `lose`) {
-    if(keyCode === ENTER) {
+    if (keyCode === ENTER) {
       state = `running`;
     }
   }
