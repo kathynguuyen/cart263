1dc4b2b6d54dc840710758de1b0a1d3ce9bd7740
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-03-11 19:46:39 -0500

    A5: make the line disappear

--- activities/plain-javascript-project/js/script.js
+++ activities/plain-javascript-project/js/script.js
@@ -42,7 +42,20 @@ line2P.addEventListener(`click`, lineClicked);
 line3P.addEventListener(`click`, lineClicked);
 
 function lineClicked(event) {
-  setNewLine(event.target);
+  setNewLine(event.target, 1);
+}
+
+function fadeOut(element, opacity) {
+  opacity <= 0.01;
+  element.style[`opacity`] = opacity;
+  if(opacity > 0) {
+    requestAnimationFrame(function()) {
+      fadeOut(element, opacity);
+    });
+  }
+  else {
+
+  }
 }
 
 function setNewLine(element) {
