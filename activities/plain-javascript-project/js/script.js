1d5f8b15ffd74fc99e4258449644634d9babdac0
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-03-11 19:33:34 -0500

    A5: Added random function

--- activities/plain-javascript-project/js/script.js
+++ activities/plain-javascript-project/js/script.js
@@ -1,11 +1,39 @@
 /**
-Title of Project
-Author Name
+Haiku Generator
+Kathy Nguyen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Generates a random haiku!
 */
 
 "use strict";
 
 // Code goes here
+
+let fiveSyllableLines = [
+  `0, to be a tree`,
+  `The cat does not know`,
+  `We are all forests`,
+  `You have done your best`,
+  `They are all gone now`
+ ];
+
+let sevenSyllableLines = [
+  `Say the things left unsaid`,
+  `Never believe the wind's lies`,
+  `The autmn stretches its legs`,
+  `Nothing can satisfy you`,
+  `They will not come back again`
+];
+
+let line1 = random(fiveSyllableLines);
+let line2 = random(sevenSyllableLines);
+let line3 = random(fiveSyllableLines);
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+
+function random(array) {
+  let index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
