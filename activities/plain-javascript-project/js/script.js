a5c4d37bb3c772c021165e80ea220f6af6165656
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-03-11 19:57:58 -0500

    A5: Added fade in function

--- activities/plain-javascript-project/js/script.js
+++ activities/plain-javascript-project/js/script.js
@@ -42,19 +42,31 @@ line2P.addEventListener(`click`, lineClicked);
 line3P.addEventListener(`click`, lineClicked);
 
 function lineClicked(event) {
-  setNewLine(event.target, 1);
+  fadeOut(event.target, 1);
 }
 
 function fadeOut(element, opacity) {
-  opacity <= 0.01;
+  opacity -= 0.01;
   element.style[`opacity`] = opacity;
   if(opacity > 0) {
-    requestAnimationFrame(function()) {
+    requestAnimationFrame(function() {
       fadeOut(element, opacity);
     });
   }
   else {
+    setNewLine(element);
+    fadeIn(element, 0);
+  }
+}
 
+
+function fadeIn(element, opacity) {
+  opacity += 0.01;
+  element.style[`opacity`] = opacity;
+  if (opacity < 1) {
+    requestAnimationFrame(function() {
+      fadeIn(element, opacity);
+    });
   }
 }
 
