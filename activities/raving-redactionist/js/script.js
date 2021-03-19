cc41c43e4945ded039a7ead2582404b79bb9fe4f
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-03-18 23:46:01 -0400

    A7: Revealing the text

--- activities/raving-redactionist/js/script.js
+++ activities/raving-redactionist/js/script.js
@@ -1,11 +1,15 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
-// Code goes here
+setInterval(revelation, 500);
+
+  function revelation() {
+    $(`.redacted`).each(attemptReveal);
+  }
+
+  function attemptReveal() {
+    let r = Math.random();
+    if (r < 0.1) {
+      $(this).removeClass(`redacted`);
+      $(this).addClass(`revealed`);
+    }
+  }
