2a598c65f352ab3ed5a2279498576bef5d2a2000
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-04-02 01:54:22 -0400

    Prototype: Added responsivevoice script

--- projects/prototype/js/script.js
+++ projects/prototype/js/script.js
@@ -1,11 +1,44 @@
 /**
-Title of Project
-Author Name
+Alice in Borderlands
+Kathy Nguyen
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+This is a prototype of the responsive voice.
+The game master will give instructions through the phone.
 */
 
-"use strict";
 
 // Code goes here
+
+let beginGame = `Welcome to the game. Are you ready to start?`
+let saying = ``;
+
+let state = `start`
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw(){
+  background(0);
+
+  if (state === `start`) {
+    start();
+  }
+}
+
+
+
+function start(){
+  speak();
+
+  push();
+  textSize(20);
+  textAlign(CENTER);
+  fill(255,255,255);
+  text(beginGame, width/2, height /2);
+  pop();
+}
+
+function speak(){
+  setTimeout(responsiveVoice.speak(beginGame, "Japanese Female"),10);
+}
