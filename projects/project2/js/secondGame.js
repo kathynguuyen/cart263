4c12f6ab594460465bf641ea168248f1a5cedac3
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-04-29 01:48:56 -0400

    P2: Second game
    - When the player inputs the word backwards it goes to the third game

--- projects/project2/js/secondGame.js
+++ projects/project2/js/secondGame.js
@@ -2,21 +2,25 @@
 let randomWords;
 
 
-let input;
+// input field
+let input, finalInput;
 
-let finalInput;
-
-
+// reveres word variables
+let result, reverseWord;
 
 function secondGame() {
   push();
   textSize(20);
   textAlign(CENTER);
   fill(255, 255, 255);
-  text(word1, width / 2, height / 2);
+
   image(phoneInstruction, 0, 0, 700, 500);
 
+  reverseWord = reverseString(word1);
+  console.log(reverseWord)
   checkInput();
+
+  text(word1, width / 2, height / 2);
 
   pop();
 }
@@ -26,9 +30,9 @@ function secondGame() {
 /**
 Reverses the provided string
 */
-function reverseString(word1) {
+function reverseString(string) {
   // Split the string into an array of characters
-  let characters = word1.split('');
+  let characters = string.split('');
   // Reverse the array of characters
   let reverseCharacters = characters.reverse();
   // Join the array of characters back into a string
@@ -39,7 +43,7 @@ function reverseString(word1) {
 
 function checkInput() {
   finalInput = input.value();
-  if (finalInput === word1) {
-    state = `death`;
+  if (finalInput === reverseWord) {
+    state = `thirdGame`;
   }
 }
