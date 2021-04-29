8b415835d87caeea8ec62c23efb0a44313bef2ad
kathynguuyen <49634908+kathynguuyen@users.noreply.github.com>
2021-04-28 22:53:35 -0400

    P2: Fixed instructions

--- projects/project2/js/firstGame.js
+++ projects/project2/js/firstGame.js
@@ -1,4 +1,4 @@
-let instructionsFirstGame = `You. have. to click ... the doors the. right. amount. of time ... from the first. to the third door. in order. Remember. this. sequence. carefully. but... backwards. Two.Three.One`;
+let instructionsFirstGame = `You. have. to click the doors the. right amount of times from the first. to the third door. in order. Remember this sequence carefully but backwards. Two.Three.One`;
 
 // images for first beginGame
 let phoneInstruction;
